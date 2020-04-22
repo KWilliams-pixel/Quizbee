f547a4fe6f4b1de6b0f0f06f74d9f8d2db05f516
K Williams <52259944+KWilliams-pixel@users.noreply.github.com>
2020-04-21 22:30:31 -0500

    Add files via upload

---
+++ build/precache-manifest.21f2d4b4dda2274f081281438740ded8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8beaea4d26c372c8552dc5e92857c19f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f25b6b99dd4bd4a7db9",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "4c7ccea165daa7e886e8",
+    "url": "/static/js/2.69ec6805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69ec6805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f25b6b99dd4bd4a7db9",
+    "url": "/static/js/main.fec19d58.chunk.js"
+  },
+  {
+    "revision": "e6f051ff9243142a8526",
+    "url": "/static/js/runtime-main.2f7cebe6.js"
+  }
+]);
